501fe939cab30db92f65c0fae887d3f8bf261494
jaballogian <jaballogianrma77@gmail.com>
2020-08-25 21:23:03 +0700

    Course 6 - ReactDOM & JSX Practice https://scrimba.com/p/p7P5Hd/cM7K3Hk

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
-import App from './Course 5 - ReactDOM & JSX/App' // change this to import own react module
+import App from './Course 6 - ReactDOM & JSX Practice/App' // change this to import own react module
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
