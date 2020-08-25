cd5c343531002f41631c24474fccc5e9e357fe9e
jaballogian <jaballogianrma77@gmail.com>
2020-08-25 21:40:54 +0700

    Course 11 - React Parent & Child Components https://scrimba.com/p/p7P5Hd/cQ726Sr

---
+++ src/Course 11 - React Parent & Child Components/Components/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MainContent(){
+    return(
+        <div>
+            <ul>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 3</li>
+            </ul>
+            <p>This is where most of my content will go...</p>
+        </div>
+    )
+}
+
+export default MainContent
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
-import App from './Course 8 - React Functional Components Practice/MyInfo' // change this to import own react module
+import App from './Course 11 - React Parent & Child Components/App' // change this to import own react module
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
