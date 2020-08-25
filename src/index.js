5708d00117ff6ae285e80750ca2f481ce77de75e
jaballogian <jaballogianrma77@gmail.com>
2020-08-25 21:49:44 +0700

    Course 13 - React Todo App - Phase 1 https://scrimba.com/p/p7P5Hd/cVwzpCp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
-import App from './Course 11 - React Parent & Child Components/App' // change this to import own react module
+import App from './Course 13 - React Todo App - Phase 1/App' // change this to import own react module
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
