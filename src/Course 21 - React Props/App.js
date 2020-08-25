1ed40e90007d7dd6ac3405ea89d92586a0b7cabd
jaballogian <jaballogianrma77@gmail.com>
2020-08-25 23:59:58 +0700

    Course 21 - React Props - version 2 (another type of props) https://scrimba.com/p/p7P5Hd/cqP7bhm

--- src/Course 21 - React Props/App.js
+++ src/Course 21 - React Props/App.js
@@ -7,30 +7,36 @@ function App(){
         <div className="contacts">
 
             <ContactCard 
-                name = "Mr. Wishkerson"
-                imgUrl = "http://placekitten.com/300/200"
-                phone = "(212) 555-1234"
-                email = "mr.whiskaz@catnap.meow"
+                contact={{
+                    name: "Mr. Wishkerson",
+                    imgUrl: "http://placekitten.com/300/200",
+                    phone: "(212) 555-1234",
+                    email: "mr.whiskaz@catnap.meow"}}
             />
 
             <ContactCard 
-                name = "Fluffykins"
-                imgUrl = "http://placekitten.com/400/200"
-                phone = "(212) 555-2345"
-                email = "fluff@me.com"
+                contact={{
+                    name: "Fluffykins",
+                    imgUrl: "http://placekitten.com/400/200",
+                    phone: "(212) 555-2345",
+                    email: "fluff@me.com"}}
             />
 
             <ContactCard 
-                name = "Destroyer"
-                imgUrl = "http://placekitten.com/400/300"
-                phone = "(212) 555-3456"
-                email = "ofworlds@yahoo.com"/>
+                contact={{
+                    name: "Destroyer",
+                    imgUrl: "http://placekitten.com/400/300",
+                    phone: "(212) 555-3456",
+                    email: "ofworlds@yahoo.com"}}
+            />
 
             <ContactCard
-                name = "Felix"
-                imgUrl = "http://placekitten.com/200/100"
-                phone = "(212) 555-4567"
-                email = "thecat@hotmail.com"/>
+                contact={{
+                    name: "Felix",
+                    imgUrl: "http://placekitten.com/200/100",
+                    phone: "(212) 555-4567",
+                    email: "thecat@hotmail.com"}}
+            />
         </div>
     )
 }
