926d9ceac2b18c4585ecf2eb416cee4af82a5b83
jaballogian <jaballogianrma77@gmail.com>
2020-08-27 16:51:57 +0700

    Course 40 - React Conditional Render - version 2 (method 2: using turnary operator) https://scrimba.com/p/p7P5Hd/c4kJNSL

--- src/Course 40 - React Conditional Render/Conditional.js
+++ src/Course 40 - React Conditional Render/Conditional.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
 function Conditional(props){
-    if(props.isLoading === true){
-        return(
-            <h1>Loading...</h1>
-        )
-    }
+
+    // method 1
+    // if(props.isLoading === true){
+    //     return(
+    //         <h1>Loading...</h1>
+    //     )
+    // }
+    // return(
+    //     <h1>Some cool stuff about conditional rendering</h1>
+    // )
+
+    // method 2 - using turnary operator
     return(
-        <h1>Some cool stuff about conditional rendering</h1>
+        <div>
+            {props.isLoading ? <h1>Loading...</h1> : <h1>Some cool stuff about conditional rendering</h1>}
+        </div>
     )
 }
 
