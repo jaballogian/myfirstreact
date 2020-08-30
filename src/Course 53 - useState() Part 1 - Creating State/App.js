f66082d12b96e56b20d938d1dc6778b11935c58c
jaballogian <jaballogianrma77@gmail.com>
2020-08-30 14:44:13 +0700

    Course 53 - useState() Part 1 - Creating State - version 2 https://scrimba.com/p/p7P5Hd/cWQw9JH2

--- src/Course 53 - useState() Part 1 - Creating State/App.js
+++ src/Course 53 - useState() Part 1 - Creating State/App.js
@@ -3,19 +3,19 @@ import React, {useState} from 'react'
 function App(){
 
     // first way to use useState
-    const answer = useState("Yes")
+    // const answer = useState("Yes")
 
     // second way to use useState
-    // const [answer] = useState("Yes")
+    const [answer] = useState("Yes")
 
     console.log(answer)
     return(
         <div>
             {/* first way to use useState */}
-            <h1>Is state important to know? {answer[0]}</h1>
+            {/* <h1>Is state important to know? {answer[0]}</h1> */}
 
             {/* second way to use useState */}
-            {/* <h1>Is state important to know? {answer}</h1> */}
+            <h1>Is state important to know? {answer}</h1>
         </div>
     )
 }
